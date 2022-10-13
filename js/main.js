3188df95d754d91fd50d37ff5be50c69ca7aa60d
Francisco Gomez <pgomezdev@gmail.com>
2022-10-12 19:30:54 -0500

    V_1.2.1 mobile menu added to navbar with js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuIconMobile = document.querySelector(".icon-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuIconMobile.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive")
 }
