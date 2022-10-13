81c7b44568b888f2e7f1f71a08602ca0408e19ae
Francisco Gomez <pgomezdev@gmail.com>
2022-10-13 17:43:46 -0500

    V_1.5.0 Details section added but it's hardcoded, theres some coments on the js trying to make a function to make the details dinamic, also i added a desciption property to the class of product

--- js/main.js
+++ js/main.js
@@ -4,35 +4,44 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuIconMobile = document.querySelector(".icon-menu");
 const shoppingCart = document.querySelector(".navbar-shopping-cart");
 const mobileMenu = document.querySelector(".mobile-menu");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shopping-cart-container");
+const detailCloseIcon = document.querySelector(".product-detail-close")
+const productDetailContainer = document.querySelector("#product-detail")
 
 const cardsContainer = document.querySelector(".cards-container")
 
 navEmail.addEventListener("click", toggleDesktopMenu);
 menuIconMobile.addEventListener("click", toggleMobileMenu);
-shoppingCart.addEventListener("click", toggleAside);
+shoppingCart.addEventListener("click", toggleShoppingCartContainer);
+detailCloseIcon.addEventListener("click", closeProductDetail)
+
 
 function toggleDesktopMenu() {
-  const isAsideClosed = aside.classList.contains("inactive");
+  const isShoppingCartContainerClosed = shoppingCartContainer.classList.contains("inactive");
+  const isProductDetailClosed = productDetailContainer.classList.contains("inactive")
 
-  if (!isAsideClosed) {
-    aside.classList.add("inactive");
+  if (!isShoppingCartContainerClosed) {
+    shoppingCartContainer.classList.add("inactive");
+  }
+  if (!isProductDetailClosed) {
+    productDetailContainer.classList.add("inactive");
   }
   desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-  const isAsideClosed = aside.classList.contains("inactive");
+  const isshoppingCartContainerClosed = shoppingCartContainer.classList.contains("inactive");
 
-  if (!isAsideClosed) {
-    aside.classList.add("inactive");
+  if (!isshoppingCartContainerClosed) {
+    shoppingCartContainer.classList.add("inactive");
   }
   mobileMenu.classList.toggle("inactive");
 }
 
-function toggleAside() {
+function toggleShoppingCartContainer() {
   const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
   const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+  const isProductDetailClosed = productDetailContainer.classList.contains("inactive")
 
   if (!isDesktopMenuClosed) {
     desktopMenu.classList.add("inactive");
@@ -40,16 +49,41 @@ function toggleAside() {
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add("inactive");
   }
-  aside.classList.toggle("inactive");
+  if (!isProductDetailClosed) {
+    productDetailContainer.classList.add("inactive");
+  }
+  shoppingCartContainer.classList.toggle("inactive");
+}
+
+function openProductDetail() {
+    const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+    const isShoppingCartContainerClosed = shoppingCartContainer.classList.contains("inactive");
+    let productSelected = [];
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add("inactive");
+    }
+    if (!isShoppingCartContainerClosed) {
+        shoppingCartContainer.classList.add("inactive");
+    }
+
+    productSelected.push()
+    // showDetails(productList)
+    productDetailContainer.classList.remove("inactive")
+}
+
+function closeProductDetail() {
+    productDetailContainer.classList.add("inactive")
 }
 
 function showProducts(arr) {
-    arr.forEach(product => {
+    arr.forEach((product) => {
         const productCard = document.createElement("div")
         productCard.classList.add("product-card")
     
         const img = document.createElement('img')
         img.setAttribute('src', product.image)
+        img.addEventListener('click', openProductDetail)
     
         const productInfo = document.createElement('div')
         productInfo.classList.add("product-info")
@@ -79,3 +113,86 @@ function showProducts(arr) {
 }
 
 showProducts(productList);
+
+/*
+function showDetails(arr) {
+    const arrayMaped = arr.map((product) => {
+        return product.name
+    })
+
+    arr.find((product) => {
+
+
+        const productDetailClose = document.createElement("div")
+        productDetailClose.classList.add("produc-detail-close")
+        
+        const xImg = document.createElement("img")
+        xImg.setAttribute("src", "/src/Icons/icon_close.png")
+        xImg.setAttribute("alt", "X")
+        
+        productDetailClose.appendChild(xImg)
+        
+        const detailsImage = document.createElement("img")
+        detailsImage.setAttribute("src", product.image)
+        detailsImage.setAttribute("alt", product.name)
+        
+        const dotsContainer = document.createElement("div")
+        dotsContainer.classList.add("dots")
+        
+        const dot1 = document.createElement("div")
+        const dot2 = document.createElement("div")
+        const dot3 = document.createElement("div")
+        dot1.classList.add("dot")
+        dot2.classList.add("dot")
+        dot3.classList.add("dot")
+        
+        dotsContainer.append(dot1, dot2, dot3)
+
+        const productInfoContainer = document.createElement("div")
+        productInfoContainer.classList.add("product-info")
+        
+        const detailPrice = document.createElement("p")
+        detailPrice.innerText = `$${product.price}`
+        const detailName = document.createElement("p")
+        detailName.innerText = product.name
+        const detailDescription = document.createElement("p")
+        detailDescription.innerText = product.description
+        
+        const cartBtn = document.createElement("button")
+        cartBtn.classList.add("primary-btn")
+        
+        const btnImg= document.createElement("img")
+        btnImg.setAttribute("src", "/src/Icons/bt_add_to_cart.svg")
+        
+        
+        cartBtn.appendChild(btnImg)
+        cartBtn.innerText = "Add to cart"
+        productInfoContainer.append(detailPrice, detailName, detailDescription, cartBtn)
+        productDetailContainer.append(productDetailClose, detailsImage, dotsContainer, productInfoContainer)
+    })
+}
+
+
+/* 
+<div class="product-detail-close">
+    <img src="src/Icons/icon_close.png" alt="x">
+</div>
+<img id="details-image" src="https://cdn.shopify.com/s/files/1/1040/9804/products/sitting_4_1024x1024.jpg?v=1560498522" alt="Corgi plushie">
+<div class="dots">
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+</div>
+<div class="product-info">
+    <p>$120,00</p>
+    <p>Corgi plushie</p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium dolores, non vero aspernatur sequi fugiat, aliquam consequatur, numquam necessitatibus ipsum doloribus quidem earum impedit tempora consectetur repudiandae! Eveniet, odio magni.
+    </p>
+    <button class="primary-btn">
+        <img src="src/Icons/bt_add_to_cart.svg" alt="Add to cart">
+        Add to cart
+    </button>
+</div>
+
+*/
--- js/products.js
+++ js/products.js
@@ -1,15 +1,16 @@
 export const productList = [];
 
 class Product {
-    constructor(name, price, image) {
+    constructor(name, price, image, description) {
         this.name = name
         this.price = price
         this.image = image
+        this.description = description
     }
 }
 
-const corgiPlushie = new Product("Corgi Plushie", 120, "https://cdn.shopify.com/s/files/1/1040/9804/products/sitting_4_1024x1024.jpg?v=1560498522")
-const pochitaPlushie = new Product("Pochita Plushie", 420, "https://bunnyhello.com/wp-content/uploads/2022/05/chaisawmanplush-12.jpg")
-const denjiNendoroid = new Product("Denji Nendoroid", 3120, "https://http2.mlstatic.com/D_NQ_NP_959761-MLC48204945494_112021-O.jpg")
+const corgiPlushie = new Product("Corgi Plushie", 120, "https://cdn.shopify.com/s/files/1/1040/9804/products/sitting_4_1024x1024.jpg?v=1560498522", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium dolores, non vero aspernatur sequi fugiat, aliquam consequatur, numquam necessitatibus ipsum doloribus quidem earum impedit tempora consectetur repudiandae! Eveniet, odio magni.")
+const pochitaPlushie = new Product("Pochita Plushie", 420, "https://bunnyhello.com/wp-content/uploads/2022/05/chaisawmanplush-12.jpg", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium dolores, non vero aspernatur sequi fugiat, aliquam consequatur, numquam necessitatibus ipsum doloribus quidem earum impedit tempora consectetur repudiandae! Eveniet, odio magni.")
+const denjiNendoroid = new Product("Denji Nendoroid", 3120, "https://http2.mlstatic.com/D_NQ_NP_959761-MLC48204945494_112021-O.jpg", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium dolores, non vero aspernatur sequi fugiat, aliquam consequatur, numquam necessitatibus ipsum doloribus quidem earum impedit tempora consectetur repudiandae! Eveniet, odio magni.")
 
 productList.push(corgiPlushie, pochitaPlushie, denjiNendoroid, pochitaPlushie, corgiPlushie, denjiNendoroid, denjiNendoroid, corgiPlushie)
